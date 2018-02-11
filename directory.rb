1b111e6024608577963415ae7adf7db5e3c23102
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-11 21:16:32 +0000

    Commented code, and code now prints total number of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# Print list of student
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Then print the total number of students
+print "Overall, we have "
+# It's important to note that print doesn't add new line characters
+print 11
+puts " great students"
