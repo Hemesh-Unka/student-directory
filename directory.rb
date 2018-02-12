2057d0f4e513cf84a808dc1181bc29ad1dfcef3c
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-12 12:50:54 +0000

    Customised code to take names via user input. Deleted already established students array

--- directory.rb
+++ directory.rb
@@ -1,18 +1,3 @@
-# Listing students in an array
-students = [
-  { name: "Dr. Hannibal Lecter", cohort: :november },
-  { name: "Darth Vader", cohort: :november },
-  { name: "Nurse Ratched", cohort: :november },
-  { name: "Michael Corleone", cohort: :november },
-  { name: "Alex DeLarge", cohort: :november },
-  { name: "The Wicked Witch of the West", cohort: :november },
-  { name: "Terminator", cohort: :november },
-  { name: "Freddy Krueger", cohort: :november },
-  { name: "The Joker", cohort: :november },
-  { name: "Joffrey Baratheon", cohort: :november },
-  { name: "Norman Bates", cohort: :november }
-]
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -28,7 +13,31 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  # empty array to hold students
+  students = []
+  # get first name
+  name = gets.chomp
+
+  # while name is not empty, repeat this block
+  while !name.empty? do
+    # add student hash to array using the shovel operator
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+
+    # ask for next student
+    name = gets.chomp
+  end
+
+  # return students array
+  students
+end
+
 # call methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
