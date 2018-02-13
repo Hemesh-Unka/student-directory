a8643afac5ca244f11ede3526de97a21a5e47c3c
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-13 12:46:32 +0000

    Added more information for user to enter for student

--- directory.rb
+++ directory.rb
@@ -6,6 +6,9 @@ end
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]})"
+    puts "Hobbies: #{student[:hobbies]}"
+    puts "Country of birth: #{student[:country_of_birth]}"
+    puts "Height: #{student[:height]}"
   end
 end
 
@@ -22,10 +25,21 @@ def input_students
   # get first name
   name = gets.chomp
 
+  puts "Please enter the students hobbies"
+  hobbies = []
+  hobby = gets.chomp
+  hobbies << hobby
+
+  puts "Please enter the students country of birth"
+  birth_country = gets.chomp
+
+  puts "Please enter the students height"
+  height = gets.chomp
+
   # while name is not empty, repeat this block
   while !name.empty? do
     # add student hash to array using the shovel operator
-    students << { name: name, cohort: :november }
+    students << { name: name, cohort: :november, hobbies: hobbies, country_of_birth: birth_country, height: height}
     puts "Now we have #{students.count} students"
 
     # ask for next student
