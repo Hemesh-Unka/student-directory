8000fe6ac3f5877094787c6f8ba904fc9d9631a8
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-17 10:10:12 +0000

    Added ability to add custom cohort month & checks to ensure month is correct

--- directory.rb
+++ directory.rb
@@ -1,3 +1,55 @@
+@months = %i[
+  january february march may june
+  july august september october november december
+]
+
+@students = [] # an empty array accessible to all methods
+
+def input_helper
+  student = {}
+  prefix = 'Please enter'
+  categories = {
+    name: "#{prefix} the name of the student",
+    cohort: "#{prefix} a cohort month (Default = :november)",
+  }
+
+  categories.each do |category, question|
+    puts question
+    if category == 'cohort'.to_sym
+
+      loop do
+        @input = gets.chomp.downcase.to_sym
+        @input = :november if @input.empty?
+        break if @months.include?(@input)
+        puts 'Please enter a valid month'
+      end
+    else
+      @input = gets.chomp
+    end
+    student[category] = @input
+  end
+  student
+end
+
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
+  student = input_helper
+
+  # while name is not empty, repeat this block
+  while !student[:name].empty? do
+    # add student hash to array using the shovel operator
+    @students << student
+
+    puts "Now we have #{@students.count} students"
+
+    student = input_helper
+  end
+
+  # return students array
+  @students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -5,35 +57,12 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]})"
+    puts "Name & Cohort: #{student[:name]} (#{student[:cohort]})"
   end
 end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-
-  # empty array to hold students
-  students = []
-  # get first name
-  name = gets.chomp
-
-  # while name is not empty, repeat this block
-  while !name.empty? do
-    # add student hash to array using the shovel operator
-    students << { name: name, cohort: :november }
-    puts "Now we have #{students.count} students"
-
-    # ask for next student
-    name = gets.chomp
-  end
-
-  # return students array
-  students
 end
 
 # call methods
